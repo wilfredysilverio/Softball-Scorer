79ba3cd55d4e2619571fd4ace493e346b1698ae6
nowelbaez2017-ux <nowelbaez2017@gmail.com>
2025-09-29 11:39:43 -0400

    Layout con menú lateral y ajustes de vistas

---
+++ Scoreboard.Web/Controllers/EstadisticasController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scoreboard.Web.Controllers
+{
+    public class EstadisticasController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Si quieres pasar un título:
+            ViewData["Title"] = "Estadísticas";
+            return View();
+        }
+    }
+}
