425c078c4fc00191ea4f8b5c1deaedcef535e719
Nowel Baez <nowelbaez2017@gmail.com>
2025-10-05 21:09:54 -0400

    Merge por áreas: visual (ours) y controles/BD (theirs)

--- Scoreboard.Web/obj/Debug/net9.0/Scoreboard.Web.AssemblyInfo.cs
+++ Scoreboard.Web/obj/Debug/net9.0/Scoreboard.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Scoreboard.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602fb2f600b5c315bc853bd8471bf599ae1bca33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca376bbaf9736cbccf3ec20322965a3cef357852")]
 [assembly: System.Reflection.AssemblyProductAttribute("Scoreboard.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Scoreboard.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
