47892429e4373487ea3ceed28574919e1acd6331
Nowel Baez <nowelbaez2017@gmail.com>
2025-11-14 22:00:52 -0400

    Agrego modelo Entrada antes del merge integracion-login-nowel

---
+++ Scoreboard.Web/Modelos/Entrada.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Scoreboard.Web.Modelos
+{
+    public class Entrada
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int PartidoId { get; set; }
+
+        // 1..9 (o más si hay extra innings)
+        [Range(1, 50)]
+        public int NumeroInning { get; set; } = 1;
+
+        // Tablero por entrada
+        public int CarrerasCasa { get; set; }
+        public int CarrerasVisita { get; set; }
+
+        // Totales por entrada (opcional, tus vistas los usan)
+        public int HitsCasa { get; set; }
+        public int HitsVisita { get; set; }
+        public int ErroresCasa { get; set; }
+        public int ErroresVisita { get; set; }
+
+        // Navegación
+        public Partido? Partido { get; set; }
+    }
+}
